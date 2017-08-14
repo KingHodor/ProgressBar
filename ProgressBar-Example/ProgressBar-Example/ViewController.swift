fab97afb4b490a80f22f7b99ece4a56fb3996a5c
Everc0in <30937159+Everc0in@users.noreply.github.com>
2017-08-14 00:57:44 -0700

    Add files via upload

---
+++ ProgressBar-Example/ProgressBar-Example/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ProgressBar-Example
+//
+//  Created by Zehra Alptekin on 14.08.2017.
+//  Copyright © 2017 evercoin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var progressBar1: ProgressBar!
+    @IBOutlet weak var progressBar2: ProgressBar!
+    @IBOutlet weak var progressBar3: ProgressBar!
+    @IBOutlet weak var progressBar4: ProgressBar!
+    @IBOutlet weak var progressBar5: ProgressBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.dance()
+    }
+
+    private func dance(){
+        self.progressBar1.allGrey()
+        self.progressBar2.allGreen()
+        self.progressBar3.startLoading()
+        self.progressBar4.startLoadingReverse()
+        self.progressBar5.allGreenReverse()
+    }
+
+}
+
