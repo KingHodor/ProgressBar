e7cb33d296aa499c0c5ab702e3dcdd8005674b24
Everc0in <30937159+Everc0in@users.noreply.github.com>
2017-08-14 00:26:06 -0700

    Add files via upload

---
+++ ProgressBar/ProgressBar/ProgressBar.h
@@ -0,0 +1,19 @@
+//
+//  ProgressBar.h
+//  ProgressBar
+//
+//  Created by Zehra Alptekin on 14.08.2017.
+//  Copyright © 2017 evercoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProgressBar.
+FOUNDATION_EXPORT double ProgressBarVersionNumber;
+
+//! Project version string for ProgressBar.
+FOUNDATION_EXPORT const unsigned char ProgressBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressBar/PublicHeader.h>
+
+
---
+++ ProgressBar/ProgressBar/ProgressBar.swift
@@ -0,0 +1,155 @@
+//
+//  ProgressBar.swift
+//  Evercoin
+//
+//  Created by Zehra Alptekin on 7.08.2017.
+//  Copyright © 2017 Evercoin. All rights reserved.
+//
+
+import UIKit
+
+class ProgressBar: UIView {
+    
+    private let arrowGreenImage = UIImage(named: "arrowGreen")
+    private let arrowGrayImage = UIImage(named: "arrowGray")
+    internal var myFrame: CGRect?
+    private var images:[UIImageView] = []
+    private let gapSize:CGFloat = 8.0
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initializeSubviews()
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.myFrame = frame
+        initializeSubviews()
+    }
+    
+    func initializeSubviews() {
+        self.layoutIfNeeded()
+        if myFrame == nil{
+            myFrame = self.frame
+        }
+        self.backgroundColor = UIColor.white
+        let frameWidth = UIScreen.main.bounds.width * (myFrame?.width)! / 375
+        let imageViewSize = Int(frameWidth / gapSize)
+        let height = (24.0 - 8.0) / 2.0
+        var index:CGFloat = 0
+        
+        for _ in 1...imageViewSize {
+            let imageView = UIImageView(image: arrowGrayImage)
+            imageView.frame = CGRect(x: Double(index), y: height, width: 6.0, height: 8.0)
+            index = index + gapSize
+            self.addSubview(imageView)
+            images.append(imageView)
+        }
+    }
+    
+    func startLoading(){
+        var index = 0
+        for imageView in images{
+            imageView.image = arrowGreenImage
+            let imageViewYPosition:CGFloat = ((24.0 - 8.0) / 2.0 ) + 4//((myFrame?.height)! - 5) / 2
+            let imageViewXPosition = imageView.frame.origin.x
+            let animation = CABasicAnimation(keyPath: "position")
+            let startingPoint = NSValue(cgPoint: CGPoint(x: imageViewXPosition, y: imageViewYPosition))
+            let endingPoint = NSValue(cgPoint: CGPoint(x: imageViewXPosition + gapSize, y: imageViewYPosition))
+            animation.fromValue = startingPoint
+            animation.toValue = endingPoint
+            animation.repeatCount = Float.greatestFiniteMagnitude
+            animation.duration = 0.5
+            imageView.layer.add(animation, forKey: "linearMovement")
+            
+            index = index + 1
+            if images.count == index{
+                let scaleAnimation: CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+                scaleAnimation.fromValue = 1.0
+                scaleAnimation.toValue = 0.0
+                scaleAnimation.repeatCount = Float.greatestFiniteMagnitude
+                scaleAnimation.duration = 0.5
+                imageView.layer.add(scaleAnimation, forKey: "scale")
+            }
+            else if index == 1{
+                let scaleAnimation: CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+                scaleAnimation.fromValue = 0.0
+                scaleAnimation.toValue = 1.0
+                scaleAnimation.repeatCount = Float.greatestFiniteMagnitude
+                scaleAnimation.duration = 0.5
+                imageView.layer.add(scaleAnimation, forKey: "scale")
+            }
+        }
+        
+    }
+    
+    func startLoadingReverse(){
+        var index = 0
+        for imageView in images{
+            imageView.image = #imageLiteral(resourceName: "arrowBackGreen")
+            let imageViewYPosition:CGFloat = ((24.0 - 8.0) / 2.0 ) + 4//((myFrame?.height)! - 5) / 2
+            let imageViewXPosition = imageView.frame.origin.x
+            let animation = CABasicAnimation(keyPath: "position")
+            let startingPoint = NSValue(cgPoint: CGPoint(x: imageViewXPosition, y: imageViewYPosition))
+            let endingPoint = NSValue(cgPoint: CGPoint(x: imageViewXPosition - gapSize, y: imageViewYPosition))
+            animation.fromValue = startingPoint
+            animation.toValue = endingPoint
+            animation.repeatCount = Float.greatestFiniteMagnitude
+            animation.duration = 0.5
+            imageView.layer.add(animation, forKey: "linearMovement")
+            
+            index = index + 1
+            if images.count == index{
+                let scaleAnimation: CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+                scaleAnimation.fromValue = 0.0
+                scaleAnimation.toValue = 1.0
+                scaleAnimation.repeatCount = Float.greatestFiniteMagnitude
+                scaleAnimation.duration = 0.5
+                imageView.layer.add(scaleAnimation, forKey: "scale")
+            }
+            else if index == 1{
+                let scaleAnimation: CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+                scaleAnimation.fromValue = 1.0
+                scaleAnimation.toValue = 0.0
+                scaleAnimation.repeatCount = Float.greatestFiniteMagnitude
+                scaleAnimation.duration = 0.5
+                imageView.layer.add(scaleAnimation, forKey: "scale")
+            }
+        }
+        
+    }
+    
+    func allGrey(){
+        let height = ((myFrame?.height)! - 8) / 2
+        var index:CGFloat = 0
+        for imageView in images {
+            imageView.image = #imageLiteral(resourceName: "arrowGray")
+            imageView.frame = CGRect(x: index, y: height, width: 6.0, height: 8.0)
+            index = index + 8
+            imageView.layer.removeAllAnimations()
+        }
+    }
+    
+    func allGreen(){
+        let height = ((myFrame?.height)! - 8) / 2
+        var index:CGFloat = 0
+        for imageView in images {
+            imageView.image = #imageLiteral(resourceName: "arrowGreen")
+            imageView.frame = CGRect(x: index, y: height, width: 6.0, height: 8.0)
+            index = index + 8
+            imageView.layer.removeAllAnimations()
+        }
+    }
+    
+    func allGreenReverse(){
+        let height = ((myFrame?.height)! - 8) / 2
+        var index:CGFloat = 0
+        for imageView in images {
+            imageView.image = #imageLiteral(resourceName: "arrowBackGreen")
+            imageView.frame = CGRect(x: index, y: height, width: 6.0, height: 8.0)
+            index = index + 8
+            imageView.layer.removeAllAnimations()
+        }
+    }
+}
